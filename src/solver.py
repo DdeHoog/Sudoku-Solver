3b7ef3bb992901b20f5c3392b52379a33fb0023e
DdeHoog <damian190085@gmail.com>
2021-06-29 12:00:31 +0200

    added recursive solving and validation

--- src/solver.py
+++ src/solver.py
@@ -14,12 +14,52 @@ HORIZONTAL_LINE = "---------------------"
 VERTICAL_LINE = "| "
 
 
-def isValid(board, number, position):
+def solve(board):
+
+    # print_board(sudokuBoard1)  # Uncomment these two lines to see the steps of the program
+    # print(HORIZONTAL_LINE)  # divided by a line for clarity
+
+    # recursion base-case
+    find = find_empty(board)
+    if not find:
+        return True  # algorithm is done
+    else:
+        row, column = find
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, column)):
+            board[row][column] = i
+
+            if solve(board):
+                return True
+
+            board[row][column] = 0
+
+    return False
+
+
+def is_valid(board, number, position):
 
     # check row
     for i in range(len(board[0])):
-        if board[position[0]][i] == number and position [1] != i:
+        if board[position[0]][i] == number and position[1] != i:
             return False
+
+    # check column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # check section
+    sec_x = position[1] // 3
+    sec_y = position[0] // 3
+
+    for i in range(sec_y*3, sec_y*3 + 3):
+        for j in range(sec_x*3, sec_x*3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+
+    return True
 
 
 def print_board(board):
@@ -42,5 +82,10 @@ def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
-                return i, j  # row, col
+                return i, j  # row, column instead of standard column, row
 
+
+print_board(sudokuBoard1)
+print("__________________________")
+solve(sudokuBoard1)
+print_board(sudokuBoard1)
